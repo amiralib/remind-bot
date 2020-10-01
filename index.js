852d937fc1de97e7689b4f311bf98aac71c2ce49
Amir Barkam <amirbarkam6@gmail.com>
2020-09-30 19:20:06 -0700

    Add check for incorrectly formatted command

--- index.js
+++ index.js
@@ -16,13 +16,18 @@ bot.on('message', msg => {
     min: 60000,
     hr: 3600000
   };
-  const regex = /^\!remind [0-9]+ (sec|min|hr)$/;
+  const commandRegex = /^!remind [0-9]+ (sec|min|hr)$/;
   const message = msg.content;
-  if (regex.test(message)) {
+  if (commandRegex.test(message)) {
     const params = message.split(' ').slice(start = 1);
     const timeout = parseInt(params[0], 10) * timeouts[params[1]];
     setTimeout(() => {
       msg.channel.send(`${params[0]} ${params[1]} has passed`);
     }, timeout);
+  } else {
+    const regex = /^!remind/
+    if (regex.test(message)) {
+      msg.channel.send('Incorrectly formatted command! The command should be of the form\n`!remind [amount of time] [sec, min, or hr]`');
+    }
   }
 });
